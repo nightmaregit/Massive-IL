06d38ebbf066a9e76ce7400f400f494732960841
Akmalfirdaus000 <akmalfirdauss000@gmail.com>
2023-12-04 13:12:20 +0700

    detail buku

--- src/pages/Detail_buku.jsx
+++ src/pages/Detail_buku.jsx
@@ -1,81 +1,80 @@
-import { useNavigate } from 'react-router-dom'
-import Navbar from '../components/Navbar';
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 const Detail_buku = () => {
-    const navigate = useNavigate()
-    return (
-      <div>
-        <Navbar />
-        <div className="mb-10">
-          <div className="bg-masuk h-24 w-auto">
-            <form className="">
-              <div className=" relative px-[350px] mr-20 pt-5">
-                <input
-                  type="search"
-                  id="default-search"
-                  className="  w-full p-4 ps-10 text-sm text-gray-900 border    focus:ring-blue-800 focus:border-blue-800 dark:bg-gray-700 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-800 dark:focus:border-blue-800"
-                  placeholder="Search judul buku..."
-                  required=""
-                />
-                <button
-                  type="submit"
-                  className="text-white absolute end-[250px]  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium  text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 h-[54px] w-[94px] "
-                >
-                  Search
-                </button>
-              </div>
-            </form>
+  const navigate = useNavigate();
+  return (
+    <>
+      <Navbar />
+      <div className="px-16 mb-5">
+        <div className="flex pt-5">
+          <div className="">
+            <img src="https://source.unsplash.com/485x675?pohon" alt="" />
           </div>
-          <div className=" grid grid-cols-2  pt-10">
-            <div className=" pl-56">
-              <img src="https://source.unsplash.com/370x450?pohon" alt="" />
+          <div className="ml-3">
+            <div className="flex">
+              <span className=" font-bold">Judul Buku :</span>
+              <p>Ekonomi Kelas XI Peminatan Ilmu Sosial </p>
+            </div>
+            <div className=" flex">
+              <span className="  font-bold">Penulis :</span>
+              <p>Endang Mulyani, Asep Nurcahyanto </p>
             </div>
             <div className="">
-              <div className="flex">
-                <span className=" font-bold">Judul buku :</span>
-                <p className="pl-3">
-                  Buku Siswa Ekonomi Untuk Kelas XI SMA dan MA Kelompok <br />{" "}
-                  Peminatan Ilmu-Ilmu Sosial (1 Jilid )
-                </p>
-              </div>
-              <div className="flex pt-2">
-                <span className="font-bold">Penulis :</span>
-                <p className="pl-3">Endang Mulyani, Asep Nurcahyanto</p>
-              </div>
-              <div className="flex pt-2">
-                <span className="font-bold">No Panggil :</span>
-                <p className="pl-3">U 330 END b 1</p>
-              </div>
-              <div className="flex pt-2">
-                <span className=" font-bold">ISBN :</span>
-                <p className="pl-3">978-623-209-311-9</p>
-              </div>
-              <div className="flex pt-2">
-                <span className=" font-bold">Cetakan :</span>
-                <p className="pl-3">Maret 2019</p>
-              </div>
-              <div className=" pr-40 pt-2">
-                <span className=" font-bold">Deskripsi :</span>
-                <p>
-                  Buku ini dirancang sebagai panduan utama bagi siswa kelas XI
-                  SMA dan MA yang memilih kelompok peminatan Ilmu-Ilmu Sosial,
-                  khususnya dalam bidang Ekonomi. Dengan pendekatan yang
-                  komprehensif dan aplikatif, buku ini menyajikan materi-materi
-                  ekonomi yang sesuai dengan kurikulum yang berlaku.
-                </p>
-              </div>
-              <div>
-                <button
-                  onClick={() => navigate("#")}
-                  className=" border mt-20 ml-96 bg-masuk w-20 h-10 text-white rounded-full hover:animate-spin"
-                >
-                  Pinjam
-                </button>
-              </div>
+              <span className=" font-bold">Deskripsi :</span>
+              <p>
+                Buku ini dirancang sebagai panduan utama bagi siswa kelas XI SMA
+                dan MA yang memilih kelompok peminatan Ilmu-Ilmu Sosial,
+                khususnya dalam bidang Ekonomi. Dengan pendekatan yang
+                komprehensif dan aplikatif, buku ini menyajikan materi-materi
+                ekonomi yang sesuai dengan kurikulum yang berlaku.{" "}
+              </p>
             </div>
           </div>
         </div>
+        <div className="pt-3">
+          <div className="border border-black ">
+            <label
+              className="flex justify-center bg-slate-800 text-white"
+              htmlFor=""
+            >
+              DETAIL BUKU
+            </label>
+            <label className="flex  bg-slate-400" htmlFor="">
+              Judul Seri     :<p className="px-3"> - </p>{" "}
+            </label>
+            <label className="flex  bg-slate-300" htmlFor="">
+              Kode Buku     :<p className="px-3"> - </p>
+            </label>
+            <label className="flex  bg-slate-400" htmlFor="">
+              Penerbit     &nbsp; : <p className="px-3"> Tiga Serangkai</p>
+            </label>
+            <label className="flex  bg-slate-300" htmlFor="">
+              Bahasa      &nbsp;  : <p className="px-3"> Indonesia</p>
+            </label>
+            <label className="flex  bg-slate-400" htmlFor="">
+              Deskripsi Fisik   : <p />
+            </label>
+            <label className="flex  bg-slate-300" htmlFor="">
+              ISBN / ISSN    : <p className="px-3"> 978-623-209-311-9</p>
+            </label>
+            <label className="flex  bg-slate-400" htmlFor="">
+              Lokasi       &nbsp;&nbsp; &nbsp; :{" "}
+              <p className="px-3"> Perpustakaan</p>
+            </label>
+            <label
+              className="flex justify-center bg-slate-800 text-white"
+              htmlFor=""
+            >
+              KETERSEDIAAN
+            </label>
+            <label className="flex  bg-slate-400" htmlFor="">
+              Jumlah Buku    : <p className="px-3"> 52</p>
+            </label>
+          </div>
+        </div>
       </div>
-    );
-}
+    </>
+  );
+};
 
-export default Detail_buku
+export default Detail_buku;
